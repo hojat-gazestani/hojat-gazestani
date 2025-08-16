dc461254d6f4c02c0ff17528179864cbd99cb823
Hojat Gazestani <39824268+hojat-gazestani@users.noreply.github.com>
2025-08-16 20:32:31 +0330

    fix style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import '../src/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const style = document.getElementById('server-side-styles');
+      if (style) style.remove();
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
